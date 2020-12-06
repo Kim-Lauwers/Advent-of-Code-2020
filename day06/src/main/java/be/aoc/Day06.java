8486071cee3d5e01e24bb14de074ff76e6ebb0ae
Kim Lauwers <kim.lauwers@acerta.be>
2020-12-06 08:30:02 +0100

    Start day 6

---
+++ day06/src/main/java/be/aoc/Day06.java
@@ -0,0 +1,18 @@
+package be.aoc;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+
+import static java.nio.file.Files.lines;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class Day06 {
+    public static void main(final String[] args) throws Exception {
+        final Path path = Paths.get(Day06.class.getClassLoader().getResource("input.txt").toURI());
+
+
+    }
+}
